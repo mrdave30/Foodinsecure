a432e40c82d408f8388e88d6dacf9ab5f2402e87
Davidson Nkoro <mrdavenkoro@gmail.com>
2022-01-28 15:28:01 +0100

    changes

--- WCA.R
+++ WCA.R
@@ -8,11 +8,13 @@
 #Region: region in which the individual lives, discrete variable with 10 levels “1”, “2“,…, “10”.
 #PhoneAccess: discrete variable with 2 levels, “1” if the individual has access to a phone and “2” if she/he does not.
 #Using R to perform the following:
-#Fit a logistic regression to model the food security status of the individual on the other variables. Please present the model output. Briefly discuss and motivate the choice of this model and indicate whether an alternative approach could be adopted.
+#Fit a logistic regression to model the food security status of the individual on the other variables. 
+#Please present the model output. Briefly discuss and motivate the choice of this model and indicate whether an alternative approach could be adopted.
 #Which variables are significant at the 95% significance level? Please justify your answer.
 #Please provide a short interpretation of the model results.
 
 #Load data 
 foodinsecure <- readxl::read_xlsx("FoodInsecurity.xlsx")
-head(foodinsecure)
-tail(foodinsecure)
+#how  many unique values we have in each column using sapply
+sapply(foodinsecure, function(x) (unique(x)))
+
