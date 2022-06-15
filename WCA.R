9c7ca9670020d02489f6f0844c57a0632247a5c3
Davidson Nkoro <mrdavenkoro@gmail.com>
2022-06-15 22:02:04 +0200

    Processing of WCA 2020 online survey

--- WCA.R
+++ WCA.R
@@ -16,6 +16,12 @@
 #install packages
 install.packages("tidyverse")
 library(tidyverse)
+install.packages("janitor")
+library(janitor)
+install.packages("writexl")
+library(writexl)
+
+
 #Load data 
 foodinsecure <- readxl::read_xlsx("FoodInsecurity.xlsx")
 #how  many unique values we have in each column using sapply
@@ -23,4 +29,28 @@ sapply(foodinsecure, function(x) (unique(x)))
 #detecting string in a column using grep
 try <- Faostat[grep("the census covered both ", Faostat$Note), ]
 try <- Faostat[grep(">=", Faostat$Note), ]
-view(try)
+fao <-read.csv("FAOSTAT_data_2-14-2022.csv")
+try <- fao[grep(">=", fao$Note), ]
+View(try)
+#__________________________________________________________
+data <- surveydata
+survey <- data %>% select(-c(CollectorNm,RespondentID,
+                               CollectorID,StartDate, EndDate,IPAddress,
+                               FirstName,LastName,
+                               CustomData1,language)) %>% 
+ remove_empty("cols")
+
+splitsurvey <- sapply(c("q0001","q0002", "q0003", "q0004",
+                        "q0005", "q0006",  "q0007",  "q0008", 
+         "q0009",  "q0010",  "q0011",  "q0012",  "q0013",  "q0014",  "q0019"), 
+       function(x) survey[startsWith(names(survey),x)],
+       simplify = FALSE)
+q1_3 <- splitsurvey[c(1:3)]
+q1.3 <- as.data.frame(q1_3)
+view(q1.3)
+#________________________________________________________________
+
+
+
+
+
