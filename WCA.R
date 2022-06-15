e32bf0789c4f215f6564622c1d4bcd86ab1da3f5
Davidson Nkoro <mrdavenkoro@gmail.com>
2022-06-15 22:14:42 +0200

    update

--- WCA.R
+++ WCA.R
@@ -1,55 +1,78 @@
-#FoodInsecurity data
-  
-#FoodInsecure: is the food security status of the individual, value 0 if the individual is food secure and 1 if she/he is food insecure
-#Age: age of the individual, continuous variable.
-#Sex: sex of the individual, discrete variable with 2 levels “female”, and “male”.
-#Education: maximum education level attained by the individual with 3 levels “primary”, “secondary” and “tertiary”.
-#EmploymentStatus: employment status of the individual, discrete variable with 2 levels “employed”, and “unemployed”.
-#Region: region in which the individual lives, discrete variable with 10 levels “1”, “2“,…, “10”.
-#PhoneAccess: discrete variable with 2 levels, “1” if the individual has access to a phone and “2” if she/he does not.
-#Using R to perform the following:
-#Fit a logistic regression to model the food security status of the individual on the other variables. 
-#Please present the model output. Briefly discuss and motivate the choice of this model and indicate whether an alternative approach could be adopted.
-#Which variables are significant at the 95% significance level? Please justify your answer.
-#Please provide a short interpretation of the model results.
+#Data processing of the WCA 2020 update survey through survey monkey. 
+#The survey comprises of 19 questions with questions and was sent 183 countries.
+#Responses received from country were processed using the code below:
 
-#install packages
+#the following packages were used for processing this data
 install.packages("tidyverse")
 library(tidyverse)
 install.packages("janitor")
 library(janitor)
 install.packages("writexl")
 library(writexl)
+install.packages("tidyxl")
+library(tidyxl)
 
+#import the dataset using xlsx_cells function from the tidyxl package and using
+#the behead function to clean the data and arrange it in a long format
+onlinesurvey <- xlsx_cells("survey.xlsx")%>% 
+  behead("up-left", "questions") %>% 
+  behead("up-left", "items") %>% 
+  behead("left", "country") %>% 
+  # behead("left", "response") %>% 
+  select(character, questions, items, country) %>% 
+  select(4,2,3,1)
 
-#Load data 
-foodinsecure <- readxl::read_xlsx("FoodInsecurity.xlsx")
-#how  many unique values we have in each column using sapply
-sapply(foodinsecure, function(x) (unique(x)))
-#detecting string in a column using grep
-try <- Faostat[grep("the census covered both ", Faostat$Note), ]
-try <- Faostat[grep(">=", Faostat$Note), ]
-fao <-read.csv("FAOSTAT_data_2-14-2022.csv")
-try <- fao[grep(">=", fao$Note), ]
-View(try)
+#the function colnames was used to assign column headers to the new processed dataset
+colnames(onlinesurvey) <- c("country","questions", "items", "responses")
+
+#to extract each processed question from 1 to 19 from the processed dataset and paste 
+#them in different excelsheets of an excel file, a for loop was written facilitate this operation
+ids <- unique(onlinesurvey$questions)
+for(i in 1:length(ids)){
+  id <- ids[i]
+  question <- data.frame(onlinesurvey[onlinesurvey$questions == id, ])
+  assign(paste("question", i, sep="."), question)
+  # or alternatively, if you wanted the data.frames to be assigned by id, 
+  # assign(id, mini.df)
+}
+sheet <- list("Anyotherinformation"=question,
+              "Q2"=question.1,
+              "Q3"=question.2,
+              "Q4"=question.3,
+              "Q5"=question.4,
+              "Q6"=question.5,
+              "Q7"=question.6,
+              "Q8"=question.7,
+              "Q9"=question.8,
+              "Q10"=question.9,
+              "Q11"=question.10,
+              "Q12"=question.11,
+              "Q13"=question.12,
+              "Q14"=question.13,
+              "Q15"=question.14,
+              "Q16"=question.15,
+              "Q17"=question.16,
+              "Q18"=question.17,
+              "Q19"=question.18)
+write_xlsx(sheet, "test.xlsx")
+
 #__________________________________________________________
-data <- surveydata
-survey <- data %>% select(-c(CollectorNm,RespondentID,
-                               CollectorID,StartDate, EndDate,IPAddress,
-                               FirstName,LastName,
-                               CustomData1,language)) %>% 
- remove_empty("cols")
+#data <- surveydata
+#survey <- data %>% select(-c(CollectorNm,RespondentID,
+#                               CollectorID,StartDate, EndDate,IPAddress,
+#                              FirstName,LastName,
+#                             CustomData1,language)) %>% 
+#remove_empty("cols")
 
-splitsurvey <- sapply(c("q0001","q0002", "q0003", "q0004",
-                        "q0005", "q0006",  "q0007",  "q0008", 
-         "q0009",  "q0010",  "q0011",  "q0012",  "q0013",  "q0014",  "q0019"), 
-       function(x) survey[startsWith(names(survey),x)],
-       simplify = FALSE)
-q1_3 <- splitsurvey[c(1:3)]
-q1.3 <- as.data.frame(q1_3)
-view(q1.3)
+#splitsurvey <- sapply(c("q0001","q0002", "q0003", "q0004",
+#                        "q0005", "q0006",  "q0007",  "q0008", 
+#         "q0009",  "q0010",  "q0011",  "q0012",  "q0013",  "q0014",  "q0019"), 
+#       function(x) survey[startsWith(names(survey),x)],
+#       simplify = FALSE)
+#q1_3 <- splitsurvey[c(1:3)]
+#q1.3 <- as.data.frame(q1_3)
+#view(q1.3)
 #________________________________________________________________
-
 
 
 
